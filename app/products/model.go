82b9ef6cb51a871902b8d7fd8bbd0b123052af01
Gilmar de Alcantara <gilmar.alcantara@hotmart.com>
2025-12-21 01:33:01 -0300

    2. Create a new model for Product Categories

--- app/products/model.go
+++ app/products/model.go
@@ -1,17 +1,20 @@
 package products
 
 import (
+	"github.com/mytheresa/go-hiring-challenge/app/categories"
 	"github.com/mytheresa/go-hiring-challenge/app/variants"
 	"github.com/shopspring/decimal"
 )
 
 // Product represents a product in the catalog.
-// It includes a unique code and a price.
+// It includes a unique code, a price, and belongs to a category.
 type Product struct {
-	ID       uint               `gorm:"primaryKey"`
-	Code     string             `gorm:"uniqueIndex;not null"`
-	Price    decimal.Decimal    `gorm:"type:decimal(10,2);not null"`
-	Variants []variants.Variant `gorm:"foreignKey:ProductID"`
+	ID         uint            `gorm:"primaryKey"`
+	Code       string          `gorm:"uniqueIndex;not null"`
+	Price      decimal.Decimal `gorm:"type:decimal(10,2);not null"`
+	CategoryID *uint           `gorm:"index"`
+	Category   *categories.Category
+	Variants   []variants.Variant `gorm:"foreignKey:ProductID"`
 }
 
 func (p *Product) TableName() string {
